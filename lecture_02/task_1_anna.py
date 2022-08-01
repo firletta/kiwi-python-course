dad05fffb5512e91ed8abc520d9d70d79c772362
Anna Alduz <anna.alduz@kiwi.com>
2022-08-01 18:55:58 +0200

    Upload

---
+++ lecture_02/task_1_anna.py
@@ -0,0 +1,8 @@
+"""
+Create a bank account program that will first let the user create a new account with a name that the user inputs.
+Then let the user input initial balance (you can omit the currency for now). You should confirm that they input a valid
+positive amount or default to 0. And then let the user either withdraw or deposit money (ask them if they would like to
+withdraw or deposit and then ask them how much). Again confirm that the user inputs valid amounts. For the withdrawal
+also confirm that they are not withdrawing more than what the balance is. And finally, return the final balance to
+the user with the name of the account.
+"""
---
+++ lecture_02/task_2_anna.py
@@ -0,0 +1,7 @@
+"""
+Create a knowledge trivia game called Bamboozled. First greet the user and explain the rules of the game.
+Then ask the user series of 5 questions and store the information whether the answer was correct or not.
+A the end of the game, return how many answers were correct and what prize the user won based on the number of
+correct answers. For example: 0 correct answers - nothing, you got Bamboozled, 1 - a meatball sandwich, 2 - a pizza,
+3 - a new pair of jeans, 4 - a Wacky Wango Card (a free ticket to try the trivia again), 5 - a Porsche.
+"""
