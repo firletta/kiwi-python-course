3372ee96bf23138d879def67a19306874e62baa5
Anna <firletta@gmail.com>
2022-08-01 21:06:48 +0200

    Finished homework.

--- lecture_02/task_1_anna.py
+++ lecture_02/task_1_anna.py
@@ -6,3 +6,33 @@ withdraw or deposit and then ask them how much). Again confirm that the user inp
 also confirm that they are not withdrawing more than what the balance is. And finally, return the final balance to
 the user with the name of the account.
 """
+
+name = input("Enter a name for your account: ")
+balance = int(input("Enter account's balance: "))
+confirmation = "Current balance of account '{}' is {}."
+
+if balance >= 0:
+    withdraw_or_deposit = input("Would you like to a) withdraw or b) deposit money? ")
+    if withdraw_or_deposit == "a" or "b":
+        if withdraw_or_deposit == "a":
+            amount = int(input("Please enter amount you'd like to withdraw: "))
+            if amount > balance:
+                print("Insufficient funds on the account.")
+            else:
+                new_balance = balance - amount
+                print("Operation completed.")
+                print(confirmation.format(name,new_balance))
+        else:
+            amount = int(input("Please enter amount you'd like to deposit: "))
+            new_balance = balance + amount
+            print("Operation completed.")
+            print(confirmation.format(name,new_balance))
+    else:
+        print("Incorrect input. You should enter 'a' or 'b'.")
+else:
+     print("Incorrect input. Amount has to be equal to or grater then 0.")
+
+
+
+    
+    
