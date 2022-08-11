e78720040121029419743308d47ca5aeb1ea1f95
Anna Alduz <anna.alduz@kiwi.com>
2022-08-11 11:13:35 +0200

    practical use

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print(range(1,10))
+languages = ['ar','cs','da','de','es','fi','fr','it','ja','ko','nl','pl','pt','ro','ru']
+for i in languages:
+    print(f"https://skypicker-pub.s3-eu-west-1.amazonaws.com/email+campaigns/General_imgs/loc_bdg/{i}_get.png")
