da5ebc83ed1acb5a7bfb3d01dbd1f495a74fae36
Anna <firletta@gmail.com>
2022-09-14 07:44:26 +0200

    working on homework

--- lecture_06_functions/assignment_07.py
+++ lecture_06_functions/assignment_07.py
@@ -1,3 +1,14 @@
 # Create a recusrive function, that will for given text prints it in reversed order.
-# TODO
+
 text = "Kiwi course"
+
+# reversed_text = text [::-1]
+# print(f"Reversed text: {reversed_text}")
+
+def reverse(text):
+    if len(text) == 0:
+        return text
+    else:
+        return reverse(text[1:]) + text[0]
+
+print(f"Reversed text: {reverse(text)}")
