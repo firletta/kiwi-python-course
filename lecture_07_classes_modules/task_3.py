8769cb81ee339b26c5b48f40f3fdc50b9b06ef8f
firletta <firletta@gmail.com>
2022-09-28 19:18:58 +0200

    homework

---
+++ lecture_07_classes_modules/task_3.py
@@ -0,0 +1,10 @@
+# Create a classes called Student and Graduate
+# Student contains name, surname and a field of study
+# Graduate contains name, surname, a field of study and a obtained title.
+# Create a method `graduate` in class `Student` which creates a new instance of the Graduate class
+# Information in the Graduate instance would originate from the student, and it would also have a parameter to determine
+# which title the student get by graduation. And return the new Graduate instance.
+
+# So lets say we have `student = Student("John", "Smith", "biology")`
+# `graduated_john = student.graduate("Mgr") should result in `graduated_john` containing Graduate
+# instance with correct title, name etc.
