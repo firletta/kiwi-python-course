e2c826f54ae4db96f898e8cc417208c38f38b0b6
firletta <firletta@gmail.com>
2022-09-28 19:14:33 +0200

    issues

--- lecture_07_classes_modules/task_1.py
+++
@@ -1,3 +0,0 @@
-# Create a class Temperature which contains the temperature value and unit
-# class should have 3 methods: convert_to_kelvins, convert_to_celsius, convert_to_fahrenheit
-# calling each method should result in re-computation of the value in Temperature class and assigning correct unit.
