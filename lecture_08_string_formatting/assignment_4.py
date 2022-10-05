69f427c3f2cdd9b873d893b34649eb072f38132f
firletta <firletta@gmail.com>
2022-10-05 19:13:40 +0200

    lecture 8

---
+++ lecture_08_string_formatting/assignment_4.py
@@ -0,0 +1,22 @@
+#TODO
+
+# Print the given table, with the rows and columns, so they are nicely aligned
+# Example of the output could look like:
+# #-----------#----------------#------------#
+# |  Country  |  Capital city  | Population |
+# #-----------#----------------#------------#
+# |Czechia    |Prague          | 10,650,000 |
+# |Slovakia   |Bratislava      | 5,450,000  |
+# |USA        |Washington, D.C.|327,200,000 |
+# |Madagascar |Antananarivo    | 25,570,000 |
+# #-----------#----------------#------------#
+
+table = [
+    # -- table header --
+    ["Country", "Capital city", "Population"],
+    # -- table rows --
+    ["Czechia", "Prague", 10650000],
+    ["Slovakia", "Bratislava", 5450000],
+    ["USA", "Washington, D.C.", 327200000],
+    ["Madagascar", "Antananarivo", 25570000],
+]
