90eeafa91bf425ec7e1153e7a65c1d1f868c1ca9
firletta <firletta@gmail.com>
2022-10-19 15:07:16 +0200

    updates

--- final_project/wordle.py
+++ final_project/wordle.py
@@ -1,6 +1,16 @@
 import random
 
 
+words_file = open("words.txt","r") # source: https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt
+words_data = words_file.read()
+words_list = words_data.split()
+# word_to_guess = random.choice(words_list)
+word_to_guess = "pills"
+number_of_rounds = 2
+
+def error_message():
+    print(f"Invalid input. It has to be a valid {len(word_to_guess)}-letter word. Try again.")
+
 class Styles:
     def green_tile(text):
         return f"\x1B[38;5;232m\x1B[48;5;42m[{text.upper()}]\033[0m"
@@ -15,30 +25,26 @@ class Styles:
         return f"\x1B[38;5;232m\x1B[48;5;15m[{text.upper()}]\033[0m"
 
 
-words_file = open("words.txt","r") # source: https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt
-words_data = words_file.read()
-words_list = words_data.split()
-word_to_guess = random.choice(words_list)
-
-
 # printing game rules
 print(f"\nWelcome to\n{Styles.green_tile('W')} {Styles.green_tile('O')} {Styles.green_tile('R')} {Styles.green_tile('D')} {Styles.green_tile('L')} {Styles.green_tile('E')}\n\n")
-print(f"How To Play:\nGuess the word in 6 tries.\n- Each guess must be a 5-letter word.\n- The color of the tiles will change to show how close your guess was to the word.\n\nExamples:")
+print(f"How To Play:\nGuess the word in {number_of_rounds} tries.\n- Each guess must be a  {len(word_to_guess)}-letter word.\n- The color of the tiles will change to show how close your guess was to the word.\n\nExamples:")
 print(f"\n{Styles.green_tile('w')} {Styles.white_tile('e')} {Styles.white_tile('a')} {Styles.white_tile('r')} {Styles.white_tile('y')}\n'W' is in the word and in the correct spot.")
 print(f"\n{Styles.white_tile('p')} {Styles.yellow_tile('i')} {Styles.white_tile('l')} {Styles.white_tile('l')} {Styles.white_tile('s')}\n'I' is in the word but in the wrong spot.")
 print(f"\n{Styles.white_tile('v')} {Styles.white_tile('a')} {Styles.white_tile('g')} {Styles.grey_tile('u')} {Styles.white_tile('e')}\n'U' is not in the word in any spot.\n")
 
-
-for round in range(1,7):
+# game below
+for round in range(1,number_of_rounds+1):
     user_input = input("Your guess: ")
     user_guess = user_input.lower()
 
-    while len(user_guess) != 5:
-        user_input = input("Invalid input. It has to be a valid 5-letter word. Try again: ")
+    while len(user_guess) != len(word_to_guess):
+        error_message()
+        user_input = input("Your guess: ")
         user_guess = user_input.lower()
 
     while user_guess not in words_list:
-        user_input = input("Invalid input. It has to be a valid 5-letter word. Try again: ")
+        error_message()
+        user_input = input("Your guess: ")
         user_guess = user_input.lower()
 
     colored_user_guess = []
@@ -55,9 +61,9 @@ for round in range(1,7):
 
     print(f"\n{round}. {' '.join(colored_user_guess)}\n")
 
-    if round == 6:
+    if round == number_of_rounds:
         print(f"Game over. The Wordle is {word_to_guess.upper()}.")
 
-    if green_tiles_count == 5:
+    if green_tiles_count == len(word_to_guess):
         print(f"Congrats! You guessed the Wordle.")
         break
